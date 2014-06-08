2c9ed44f5d7445403bd930f763777118278951cd
Ryan Wiemer <ryan@ryanwiemer.com>
2014-06-07 17:07:22 -0700

    Setting up dependicies

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+// Include gulp
+var gulp = require('gulp');
+
+// Include the Plugins
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var minifycss = require('gulp-minify-css');
+var notify = require ('gulp-notify');
+var lr = require('tiny-lr');
+var livereload = require('gulp-livereload');
+
+
+// Move and Minfiy Scripts from Bower
+gulp.task ('move', function() {
+    return gulp.src(['bower_components/jquery/dist/jquery.js'])
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(uglify())
+        .pipe(gulp.dest('assets/js/'))
+        .pipe(notify({ message: 'Bower components are moved!'}));
+});
+
+
+// Concatenate, Lint and Minify JS
+gulp.task('scripts', function() {
+    return gulp.src(['assets/js/scripts/*.js'])
+        .pipe(concat('global.js'))
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(uglify())
+        .pipe(rename('global.min.js'))
+        .pipe(gulp.dest('assets/js/'))
+        //.pipe(notify({ message: 'Scripts are minified!'}))
+        .pipe(livereload());
+});
+
+// Compile Sass & Minify CSS
+gulp.task('sass', function() {
+    return gulp.src(['assets/scss/style.scss'])
+        .pipe(sass())
+        .pipe(minifycss())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('assets/css/'))
+        //.pipe(notify({ message: 'CSS is minified!'}))
+        .pipe(livereload());
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('assets/scss/*/*.scss', ['sass'])
+    gulp.watch('assets/js/*/*.js', ['scripts']);
+});
+
+// Default Task
+gulp.task('default', ['scripts', 'sass', 'watch']);
