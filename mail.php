8e4b8a1e1f5e23945f7d8ebe110797edad2dbe55
Ryan Wiemer <ryan@ryanwiemer.com>
2014-08-02 20:34:20 -0700

    akismet

--- mail.php
+++ mail.php
@@ -1,25 +1,31 @@
 <?php
 
+    require_once('../../../wp-load.php');
+    require_once('Akismet.class.php');
     $to = get_option( 'admin_email' );
-    $from = preg_replace("([\r\n])", "", $_REQUEST['email']);
-    $name = preg_replace("([\r\n])", "", $_REQUEST['name']);
-    $headers = "From: $from";
-    $subject = "Contact Form Inquiry From $from";
+    $name =  sanitize_text_field( $_POST['name'] );
+    $subject_line = sanitize_text_field( $_POST['subject'] );
+    $subject = "$name | $subject_line";
+    $headers = "From: " . sanitize_email( $_POST["email"] ) . "\r\n";
+    $headers .= "Reply-To: ". sanitize_email( $_POST["email"] ) . "\r\n";
+    $body = esc_textarea( $_POST["message"] );
 
-    $fields = array();
-    $fields{"name"} = "name";
-    $fields{"email"} = "email";
-    $fields{"message"} = "message";
 
-    $body = "Here is what was sent:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
+    if (isset($email) &&  !empty($email)) {
 
-    $match = "/(bcc:|cc:|content\-type:)/i";
-if (preg_match($match, $_REQUEST['email']) ||
-    preg_match($match, $_REQUEST['message']) ||
-    preg_match($match, $_REQUEST['name'])) {
-  die("Header injection detected.");
-}
-
+    $apikey = 'ba8bb1c44043';
+    $blogurl = 'http://stage.ryanwiemer.com/';
+    $akismet = new Akismet($blogurl ,$apikey);
+    $akismet->setCommentAuthor($name);
+    $akismet->setCommentAuthorEmail($email);
+    $akismet->setCommentContent($message);
+    if($akismet->isCommentSpam()) {
+    $myFile = "spam.txt";
+    $fh = fopen($myFile, 'a') or die("can't open spam file");
+    $stringData = sprintf("Name: %s\r\nEmail: %s \r\nMessage: %s\r\n------------------------------------\r\n",$name,$email,$want,$message);
+    fwrite($fh, $stringData);
+    fclose($fh);
+    } else {
     $send = wp_mail($to, $subject, $body, $headers);
-
+  }
 ?>
