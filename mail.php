ea069856f13164a3fdb955f7abe93fd8aa97ef4c
Ryan Wiemer <ryan@ryanwiemer.com>
2015-04-06 22:38:45 -0700

    One more fix to mail.php

--- mail.php
+++ mail.php
@@ -2,6 +2,7 @@
     require_once('../../../wp-load.php');
     $to = get_option( 'admin_email' );
     $name =  sanitize_text_field( $_POST['name'] );
+    $subject_line =  sanitize_text_field( $_POST['subject'] );
     $subject = $name . " | " . $subject_line;
     $headers = "From: " . sanitize_email( $_POST["email"] ) . "\r\n";
     $headers .= "Reply-To: ". sanitize_email( $_POST["email"] ) . "\r\n";
