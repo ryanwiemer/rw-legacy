34e401314bf67abd47c1d864f864ae5786f48566
Ryan Wiemer <ryan@ryanwiemer.com>
2015-05-02 17:57:18 -0700

    Updating NPM packages and adding Browswer Sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 // Include gulp
 var gulp = require('gulp');
 
-// Include the Plugins
+// Include The Plugins
 var jshint = require('gulp-jshint');
 var sass = require('gulp-sass');
 var bourbon = require('node-bourbon');
@@ -10,56 +10,72 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var minifycss = require('gulp-minify-css');
-var lr = require('tiny-lr');
-var livereload = require('gulp-livereload');
 var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync');
+var reload      = browserSync.reload;
+
+gulp.task('browser-sync', function() {
+  //watch files
+  var files = [
+    'assets/css/*.css',
+    'assets/js/*.js',
+    '*.php'
+  ];
+  //initialize browsersync
+  browserSync.init(files, {
+  //browsersync with a php server
+  proxy: "ryanwiemer.dev",
+  notify: false
+  });
+});
 
 // Move and Minfiy Scripts from Bower
 gulp.task ('move', function() {
-    gulp.src(['bower_components/picturefill/dist/picturefill.js','bower_components/jquery/dist/jquery.js','bower_components/jquery-validate/dist/jquery.validate.js', 'bower_components/fastclick/lib/fastclick.js', 'bower_components/jquery-form/jquery.form.js'])
-        .pipe(uglify())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('assets/js/vendor/'))
+  gulp.src(['bower_components/picturefill/dist/picturefill.js','bower_components/jquery/dist/jquery.js','bower_components/jquery-validate/dist/jquery.validate.js', 'bower_components/fastclick/lib/fastclick.js', 'bower_components/jquery-form/jquery.form.js'])
+    .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('assets/js/vendor/'))
 });
 
 // Lint JS
 gulp.task('scripts', function() {
-    gulp.src(['assets/js/scripts/*.js'])
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'))
-        .pipe(livereload());
+  gulp.src(['assets/js/scripts/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(browserSync.reload({stream:true}));
 });
 
 // Concat JS
 gulp.task('concat', function() {
-    gulp.src(['assets/js/vendor/picturefill.min.js','assets/js/vendor/jquery.min.js','assets/js/scripts/global.js','assets/js/vendor/prism.min.js'])
-        .pipe(concat('scripts.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('assets/js/'));
-    gulp.src(['assets/js/vendor/jquery.form.min.js','assets/js/scripts/jquery.form.settings.js','assets/js/vendor/jquery.validate.min.js'])
-        .pipe(concat('contact.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('assets/js/'));
+  gulp.src(['assets/js/vendor/picturefill.min.js','assets/js/vendor/jquery.min.js','assets/js/scripts/global.js','assets/js/vendor/prism.min.js'])
+    .pipe(concat('scripts.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('assets/js/'));
+  gulp.src(['assets/js/vendor/jquery.form.min.js','assets/js/scripts/jquery.form.settings.js','assets/js/vendor/jquery.validate.min.js'])
+    .pipe(concat('contact.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('assets/js/'));
 });
 
 // Compile Sass & Minify CSS
 gulp.task('sass', function() {
-    gulp.src(['assets/scss/style.scss'])
-        .pipe(sass({
-            includePaths: require('node-neat').includePaths
-         }))
-        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
-        .pipe(minifycss())
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('assets/css/'))
-        .pipe(livereload());
+  gulp.src(['assets/scss/style.scss'])
+    .pipe(sass({
+        includePaths: require('node-neat').includePaths
+     }))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+    .pipe(minifycss())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('assets/css/'))
+    .pipe(browserSync.reload({stream:true}));
 });
 
 // Watch Files For Changes
 gulp.task('watch', function() {
     gulp.watch('assets/scss/*/*.scss', ['sass'])
     gulp.watch('assets/js/*/*.js', ['scripts','concat']);
+    gulp.watch('.php').on('change', reload);
 });
 
 // Default Task
-gulp.task('default', ['scripts', 'sass','concat','watch']);
+gulp.task('default', ['scripts', 'sass','concat','browser-sync','watch']);
