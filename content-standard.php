9e57b35130118c58f0d6dcf1a4c2a241cd49fcd4
Ryan Wiemer <ryan@ryanwiemer.com>
2015-10-09 00:13:01 -0700

    More testing and playing around with designs

--- content-standard.php
+++ content-standard.php
@@ -7,7 +7,7 @@
 <article class="project">
 	<a href="<?php the_permalink(); ?>">
 		<?php if ( has_post_thumbnail() ) {$medium = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'medium');?>
-		<img src="<?php echo $medium[0]; ?>"><?php } else {echo '<img src="http://placehold.it/350x150"/>';}?>
+		<img src="<?php echo $medium[0]; ?>"><?php } else {echo '<img src="http://placehold.it/1500x1125"/>';}?>
 		<h2><?php the_title(); ?></h2>
 		<h3><?php $category = get_the_category(); echo $category[0]->cat_name; ?></h3>
 	</a>
--- pages/Info.php
+++ pages/Info.php
@@ -8,19 +8,15 @@ get_header(); ?>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <section class="intro">
   <a href="#info"></a>
-    <p class="intro__text">
-      Ryan Wiemer<br><span>Digital Project Manager</span>
-    </p>
-    <div class="intro__scroll">
-        <em>scroll down</em>
-    </div>
+  <p class="intro__text">Ryan Wiemer<br><span>Digital Project Manager</span></p>
+  <div class="intro__scroll"><span>Scroll Down</span></div>
 </section> <!-- Intro -->
 <div class="container">
 
 <section id="info" class="info">
   <div class="info__bio">
     <p>
-      My name is Ryan Wiemer and I am a project manager working in San Francisco, California. I particularly enjoy working on interactive and web focused projects. I have expertsie strategic planning, technial problem solving and front end web development. I currently work at <a href="#">Epsilon</a>, a global marketing agency, where I have managed proejcts for clients including Pacific Union, ProSight Specialty Insurance, RSA, and American Express just to name a few.
+      My name is Ryan Wiemer and I am a project manager working in San Francisco, California. I particularly enjoy working on interactive and web focused projects. I have expertise in strategic planning, technical problem solving and front end web development. I currently work at <a href="#">Epsilon</a>, a global marketing agency, where I have managed proejcts for clients including Pacific Union, ProSight Specialty Insurance, RSA, and American Express just to name a few.
     </p>
     <p>
       When not working I can usually be found reading up on the latest tech gadgets, exploring the San Francisco Bay Area, or out taking photographs. I love meeting new people with similar passions or goals in making a difference on the web. If you are interested in possible work or project opportunities please feel free to get in touch.
@@ -32,7 +28,7 @@ get_header(); ?>
       <li><a href="https://instagram.com/ryanwiemer/" target="_blank">Instagram</a></li>
     </ul>
   </div>
-  <img class="info__image" src='assets/img/about.jpg' />
+  <img class="info__image" src='http://placehold.it/600x600' />
 </section>
 
 <?php the_content(); ?>
