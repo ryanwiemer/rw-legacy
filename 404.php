c92ccc6f841a249b02c3e1d4101770604cb1bf14
Ryan Wiemer <ryan@ryanwiemer.com>
2015-10-10 15:06:29 -0700

    removing unneeded markup on 404 page

--- 404.php
+++ 404.php
@@ -5,12 +5,10 @@
 get_header(); ?>
 
 <div class="container">
-		<section class="not-found">
-				<h2>Eror 404!</h2>
-				<div class="video-container">
-					<img src="<?php echo site_url(); ?>/wp-content/uploads/2015/10/twilight_zone.gif"/>
-				</div>
-				<h3>Oops! That page can&rsquo;t be found. Please check the URL and try again.</h3>
-		</section><!-- .error-404 -->
+	<section class="not-found">
+		<h2>Eror 404!</h2>
+		<img src="<?php echo site_url(); ?>/wp-content/uploads/2015/10/twilight_zone.gif"/>
+		<h3>Oops! That page can&rsquo;t be found. Please check the URL and try again.</h3>
+	</section><!-- .not-found -->
 </div> <!-- container -->
 <?php get_footer(); ?>
