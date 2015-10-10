30cf1371c07df1eea51b332ed58953ccf1434468
Ryan Wiemer <ryan@ryanwiemer.com>
2015-10-10 15:03:16 -0700

    added 404 page

--- 404.php
+++ 404.php
@@ -4,11 +4,13 @@
 	*/
 get_header(); ?>
 
-<div class="wrapper">
 <div class="container">
-		<section class="error-404 not-found">
-				<p><?php _e( 'Oops! That page can&rsquo;t be found. Please check the URL and try again.'); ?></p>
+		<section class="not-found">
+				<h2>Eror 404!</h2>
+				<div class="video-container">
+					<img src="<?php echo site_url(); ?>/wp-content/uploads/2015/10/twilight_zone.gif"/>
+				</div>
+				<h3>Oops! That page can&rsquo;t be found. Please check the URL and try again.</h3>
 		</section><!-- .error-404 -->
 </div> <!-- container -->
-</div> <!-- wrapper -->
 <?php get_footer(); ?>
--- README.md
+++ README.md
@@ -2,12 +2,6 @@
 #README
 WordPress theme built for Ryan Wiemer's personal website.
 
-###Dependencies
-- Git
-- Node.js
-- Gulp
-- Bower
-
 ###Copyright
 - All rights reserved
 - Copyright 2015 Ryan Wiemer
