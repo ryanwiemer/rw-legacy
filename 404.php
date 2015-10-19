d135296e068c46cbbb846e28b6592ad3938b32fd
Ryan Wiemer <ryan@ryanwiemer.com>
2015-10-18 19:25:34 -0700

    Cleaning up the 404 page

--- 404.php
+++ 404.php
@@ -6,7 +6,7 @@ get_header(); ?>
 
 <div class="container">
 	<h2 class="error404__title">Error 404!</h2>
-	<p class="error404__message">Oops! That page can&rsquo;t be found. Please check the URL and try again. Or return to the homepage by clicking <a href="<?php echo site_url(); ?>">here</a>.</p>
-	<img class="error404__image" src="<?php echo site_url(); ?>/wp-content/uploads/2015/10/twilight_zone.gif"/>
-</div> <!-- container -->
+		<p class="error404__message">Oops! That page can&rsquo;t be found. Please check the URL and try again. To return to the homepage click <a href="<?php echo site_url(); ?>">here</a>.</p>
+	<img class="error404__image" src="<?php echo site_url(); ?>/wp-content/uploads/2015/10/404.png"/>
+</div>
 <?php get_footer(); ?>
